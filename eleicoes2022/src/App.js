27c73225ff1fc9321d869f90207947b1d4bd7e0d
Carlos Trevisan <carlos.trevisan.dev@gmail.com>
2022-10-02 18:29:35 -0300

    final

--- eleicoes2022/src/App.js
+++ eleicoes2022/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [cand, setCand] = useState();
+  const [apur, setApur] = useState();
 
   const retrieveData = async () => {
     const result = await fetch(
@@ -11,12 +12,14 @@ function App() {
     );
 
     const candRes = await result.json();
-    console.log("rodei");
     setCand(candRes.cand);
+    setApur(candRes.pst);
   };
 
   const lula = cand ? cand[1] : "";
   const bolso = cand ? cand[0] : "";
+
+  const formatApur = apur?.replace(",", ".");
 
   useEffect(() => {
     setInterval(() => {
@@ -26,9 +29,75 @@ function App() {
 
   return (
     <div className="App">
+      <div
+        style={{
+          backgroundColor: "green",
+          position: "absolute",
+          top: 0,
+          zIndex: -1,
+          width: `${formatApur}%`,
+          height: 50,
+        }}
+      ></div>
+      <div>
+        <p>APURADOS: {apur}%</p>
+      </div>
+
       <header className="App-header">
-        <p>LULA: "{lula?.pvap}%"</p>
-        <p>BOLSO: "{bolso?.pvap}%"</p>
+        <div
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: 20,
+            }}
+          >
+            <div>
+              <p style={{ fontSize: 50, color: "black", width: 400 }}>LULA: </p>
+              <p style={{ fontSize: 50, color: "red" }}>{lula?.pvap}%</p>
+            </div>
+            <img
+              src="https://static.poder360.com.br/2022/04/Lula-Alckmin-PSB-Congresso-PT-Campanha-Eleicoes2022-1-scaled.jpg"
+              style={{
+                width: 300,
+                height: 300,
+                borderRadius: 150,
+                borderWidth: 1,
+              }}
+            ></img>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <p style={{ fontSize: 50, color: "black", width: 400 }}>
+                B
+                <text style={{ fontSize: 50, color: "#cfcb0c", width: 400 }}>
+                  O
+                </text>
+                LSO:
+              </p>
+              <p style={{ fontSize: 50, color: "green" }}>{bolso?.pvap}%</p>
+            </div>
+            <img
+              src="https://media.gettyimages.com/photos/president-of-brazil-jair-bolsonaro-adjusts-his-protective-mask-during-picture-id1207623646?s=612x612"
+              style={{
+                width: 300,
+                height: 300,
+                borderRadius: 150,
+                borderWidth: 1,
+              }}
+            ></img>
+          </div>
+        </div>
       </header>
     </div>
   );
